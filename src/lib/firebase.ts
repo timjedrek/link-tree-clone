55bc9afb1f3ff64df8674fa68c5ea0ea06fde110
Tim Jedrek <tim@timjedrek.com>
2023-10-30 11:53:26 -0500

    complete auth store.. needed to do imports to make work

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 //import { getAnalytics } from "firebase/analytics";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyTEB9Yy2-o_DUYctaVe687hccCSSssSw",
@@ -22,3 +23,31 @@ export const storage = getStorage();
 //export const analytics = getAnalytics(app);
 // ANALYTICS threw a reference window error.. check out https://joyofcode.xyz/sveltekit-google-analytics
 
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { auth } from "$lib/firebase";
-
   import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+  import { user } from "$lib/firebase";
 
   async function signInWithGoogle() {
       const provider = new GoogleAuthProvider();
@@ -13,4 +13,10 @@
 
 <h2>Login</h2>
 
-<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{#if $user}
+  <h2 class="card-title">Welcome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are logged in</p>
+  <button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
+  <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if}
