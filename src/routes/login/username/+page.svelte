15887864483734b9c966a941e3f842253e47e6cd
Tim Jedrek <tim@timjedrek.com>
2023-10-30 12:52:34 -0500

    add username validation

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -7,6 +7,12 @@
   let loading = false;
   let isAvailable = false;
 
+
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  
+  $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+  $: isTouched = username.length > 0;
+  $: isTaken = isValid && !isAvailable && !loading
   
   let debounceTimer: NodeJS.Timeout;
 
@@ -30,7 +36,28 @@
   }
 
   async function confirmUsername() {
-    // TODO
+    console.log("confirming username", username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, "usernames", username), { uid: $user?.uid });
+    batch.set(doc(db, "users", $user!.uid), { 
+      username, 
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: 'I am the Walrus',
+      links: [
+        {
+          title: 'Test Link',
+          url: 'https://kung.foo',
+          icon: 'custom'
+        }
+      ]
+    });
+
+    await batch.commit();
+
+    username = '';
+    isAvailable = false;
+
   }
 
 </script>
@@ -40,18 +67,37 @@
 <AuthCheck>
     <h2>Username</h2>
     <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
-        <input
-          type="text"
-          placeholder="Username"
-          class="input w-full"
-          bind:value={username}
-          on:input={checkAvailability}
-        />
-
-        <p>Is available? {isAvailable}</p>
-
-        <button class="btn btn-success">Confirm username @{username} </button>
-
-      </form>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input w-full"
+        bind:value={username}
+        on:input={checkAvailability}
+        class:input-error={(!isValid && isTouched)}
+        class:input-warning={isTaken}
+        class:input-success={isAvailable && isValid && !loading}
+      />
+      <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <p class="text-secondary">Checking availability of @{username}...</p>
+        {/if}
+    
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            must be 3-16 characters long, alphanumeric only
+          </p>
+        {/if}
+    
+        {#if isValid && !isAvailable && !loading}
+          <p class="text-warning text-sm">
+            @{username} is not available
+          </p>
+        {/if}
+    
+        {#if isAvailable}
+          <button class="btn btn-success">Confirm username @{username} </button>
+        {/if}
+      </div>
+    </form>
 
 </AuthCheck>
